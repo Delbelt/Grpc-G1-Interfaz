028e1fb2ff02c1451072eb93882a4965025484f2
Nahuel Flores <nahuelhernanflores25@gmail.com>
2024-10-09 21:33:22 -0300

    REF: add routes

--- src/utils/modules.ts
+++ src/utils/modules.ts
@@ -1,14 +1,37 @@
 export const modules = [
   {
+    name: "Tiendas",
     module: "Stores",
   },
   {
+    name: "Productos",
     module: "Products",
   },
   {
+    name: "Stocks",
     module: "Stocks",
   },
   {
+    name: "Usuarios",
     module: "Users",
+  },
+  {
+    name: "Novedades",
+    module: "news",
+  },
+  {
+    name: "Ordenes",
+    module: "orders",
+  },
+];
+
+export const modulesProvider = [
+  {
+    name: "Productos",
+    module: "Products",
+  },
+  {
+    name: "Ordenes",
+    module: "purchase-order",
   },
 ];
